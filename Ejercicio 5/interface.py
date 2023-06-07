f28dfdff72af9c815684aec31d628be6330a57e9
Licha_31 <79867014+Licha31@users.noreply.github.com>
2023-06-06 21:57:09 -0300

    Delete Ejercicio 5 directory

--- Ejercicio 5/interface.py
+++
@@ -1,9 +0,0 @@
-from zope.interface import Interface
-from zope.interface import implementer
-class elemento(Interface):
-    def insertarElemento(elem):
-        pass
-    def agregarElemento(elem):
-        pass
-    def mostrarElemento(i):
-        pass
