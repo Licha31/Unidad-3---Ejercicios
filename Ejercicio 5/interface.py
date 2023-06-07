9abd646c65f199544929f4e260965f5097a5d620
Licha_31 <79867014+Licha31@users.noreply.github.com>
2023-06-06 22:04:02 -0300

    Add files via upload

---
+++ Ejercicio 5/interface.py
@@ -0,0 +1,8 @@
+from zope.interface import Interface
+class elemento(Interface):
+    def insertarElemento(elem):
+        pass
+    def agregarElemento(elem):
+        pass
+    def mostrarElemento(i):
+        pass
